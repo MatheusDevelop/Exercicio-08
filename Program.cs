a6dbb207ec81aad5ff7595b7366d5490a480fee5
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-06-02 17:31:32 -0300

    Subindo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula08Abstracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+
+            CartaoCredito visa = new CartaoCredito();
+            visa.limite = 6000f;
+
+            Console.WriteLine("Quanto acrescentar no limite?");
+            float acres = float.Parse( Console.ReadLine() );
+            
+            visa.AumentarLimite(acres);
+            Console.WriteLine("Novo limite: R$ "+visa.limite);
+
+            Console.WriteLine("Digite a data do pagamento:");
+            visa.data = DateTime.Parse( Console.ReadLine() );
+            
+            Console.WriteLine("Data: " + visa.data);
+
+
+            
+        }
+    }
+}
